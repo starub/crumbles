71b761f92beec38098028c623ed73cb71e1c76e6
Stanislav Rubens <starub@protonmail.ch>
2015-03-08 18:14:18 +0100

    client test added;

--- client/tests.py
+++ client/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from client.models import Client
+
+
+class ClientTestCase(TestCase):
+
+    def test_create_client(self):
+        client = Client()
+        client.first_name = 'John'
+        client.last_name = 'Doe'
+        client.email = 'john.doe@test.com'
+        client.phone = '22133441144'        
+        
+        client.save()
+        
+        self.assertTrue(Client.objects.filter(first_name = 'John') != None)
