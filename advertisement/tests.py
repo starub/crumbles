28e98877903b6f1db6c7cc85be928333f7bc4b9b
Stanislav Rubens <starub@protonmail.ch>
2015-03-08 18:33:14 +0100

    unfinished test

--- advertisement/tests.py
+++ advertisement/tests.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+from django.db.models.sql.datastructures import DateTime
 from django.test import TestCase
 
-# Create your tests here.
+from advertisement.models import Advertisement
+from client.models import Client
+
+
+class AdvertisementTestCase(TestCase):
+
+    def test_create_multiple_advertisements(self):
+        client = Client(first_name='John', last_name='Doe')
+        
+        adv1 = Advertisement()
+        adv2 = Advertisement()
+        
+        adv1.save()
+        adv2.save()
